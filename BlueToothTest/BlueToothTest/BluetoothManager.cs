983b1b582750dc3bf20f9fb34c6deef56403a60f
houtza <houtza@uni.edu>
2018-12-17 21:30:36 -0600

    Adding the app

---
+++ BlueToothTest/BlueToothTest/BluetoothManager.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+using Java.Util;
+
+namespace BlueToothTest
+{
+    class BluetoothManager
+    {
+        //Unique ID which helps us connect to any device 
+        private const string UuidUniverseProfile = "00001101-0000-1000-8000-00805f9b34fb";
+        //Represent bluetooth data coming from UART
+        private BluetoothDevice result;
+        //get input/output stream of this comunication
+        private BluetoothSocket mSocket;
+        //convert byte[] to readable strings
+        private BufferedReader reader;
+        //private BufferedWriter writer;//----------------------
+        private System.IO.Stream mStream;
+        private InputStreamReader mReader;
+
+        private System.IO.Stream oStream;
+        //private OutputStreamWriter oWriter;//-------------------
+        
+
+
+        public BluetoothManager()
+        {
+            reader = null;
+            //writer = null;//---------------------------
+
+        }
+
+
+
+        public UUID getUUIDFromString()
+        {
+            return UUID.FromString(UuidUniverseProfile);
+        }
+
+
+
+        private void close(IDisposable aConnectableObject)
+        {
+            if (aConnectableObject == null) return;
+            try
+            {
+                aConnectableObject.Dispose();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            aConnectableObject = null;
+        }
+
+
+        public String getDataFromDevice()
+        {
+            return reader.ReadLine();
+        }
+
+        //public void sendDataToDevice(String data)
+        //{
+           // writer.Write(data.ToCharArray());//Wow cant belive that worked!!------------------------
+        //}
+
+
+        private void openDeviceConnection(BluetoothDevice btDevice)
+        {
+            try
+            {
+                //Getting socket from specific device
+                mSocket = btDevice.CreateRfcommSocketToServiceRecord(getUUIDFromString());
+                //blocking operation
+                mSocket.Connect();
+                //input stream
+                mStream = mSocket.InputStream;
+                //output stream
+                //mSocket.OutputStream;
+                oStream = mSocket.OutputStream;//----------------------------------
+
+               // oWriter = new OutputStreamWriter(oStream);//--------------------------
+                //writer = new BufferedWriter(oWriter);//-------------------------------
+
+                mReader = new InputStreamReader(mStream);
+                reader = new BufferedReader(mReader);
+            }
+            catch(IOException e)
+            {
+                //Close all
+                close(mSocket);
+                close(mStream);
+                close(mReader);
+                close(oStream);
+               // close(oWriter);----------------------------------
+                throw e;
+            }
+        }
+
+
+
+        public void getAllPairedDevices()
+        {
+            //your android phone bluetooth device
+            BluetoothAdapter btAdapter = BluetoothAdapter.DefaultAdapter;
+
+            var devices = btAdapter.BondedDevices;
+            if(devices != null && devices.Count > 0)
+            {
+                foreach (BluetoothDevice mDevice in devices)
+                {
+                    //search threw all paired devices
+                    //mDevice.Name.Split(' ');
+                    openDeviceConnection(mDevice);
+                }
+            }
+        }
+    }
+}
---
+++ BlueToothTest/BlueToothTest/MainActivity.cs
@@ -0,0 +1,81 @@
+﻿using System;
+//using System.Threading;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+using System.Threading.Tasks;
+
+
+namespace BlueToothTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private String dataPast = null;
+        private String data = null;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            BluetoothManager manager = new BluetoothManager();
+            manager.getAllPairedDevices();
+
+            TextView currentCharacterName = FindViewById<TextView>(Resource.Id.text);
+            currentCharacterName.Text = "Your Text";
+
+
+
+            System.Threading.Thread thread = new System.Threading.Thread(() =>
+            {
+                while (true)
+                {
+                    data = manager.getDataFromDevice();
+                    //manager.sendDataToDevice("H");//--
+                    
+                    RunOnUiThread(async () => { currentCharacterName.Text = data; });
+                }
+            });
+
+            thread.IsBackground = true;
+            thread.Start();
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+	}
+}
+
